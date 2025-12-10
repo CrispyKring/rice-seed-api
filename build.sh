44ab6995b332867abc00f1424e9a74c184f291f4
Carlos Hacildo <carlos.hacildocarlos@gmail.com>
2025-12-10 18:12:50 +0800

    Fix: Force PHP runtime in Render

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-composer install --no-dev --optimize-autoloader
-php artisan key:generate
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-php artisan storage:link
