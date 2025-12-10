f9cf66625a97eb412e46318f1e56c06d06003067
Carlos Hacildo <carlos.hacildocarlos@gmail.com>
2025-12-10 17:49:59 +0800

    Add Render deployment files

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+composer install --no-dev --optimize-autoloader
+php artisan key:generate
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan storage:link
